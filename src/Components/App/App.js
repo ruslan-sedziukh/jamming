1ccbd2f3f2615968d61b7e10b17ca5a2248b6211
Ruslan Sedziukh <ruslansedziukh@Katerynas-MacBook-Pro.local>
2022-05-12 13:43:16 +0300

    Project just initiated. Base files setuped.

---
+++ src/Components/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+          {
+            // Add a SearchBar component
+          }
+          <div class="App-playlist">
+            {
+              // Add a SearchResults component
+              // Add a Playlist component
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
