3fa27bf3afb841286bcbac83135204b431c4e7dc
Ruslan Sedziukh <ruslansedziukh@Katerynas-MacBook-Pro.local>
2022-05-13 13:47:42 +0300

    73

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,11 +3,21 @@ import './SearchBar.css';
 
 
 export class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(e) {
+    let searchTerm = document.querySelector(".SearchBar input").value;
+    this.props.onSearch(searchTerm);
+  }
+
   render() {
     return (
     <div className="SearchBar">
       <input placeholder="Enter A Song, Album, or Artist" />
-      <button className="SearchButton">SEARCH</button>
+      <button className="SearchButton" onClick={this.handleSearch}>SEARCH</button>
     </div>
     );
   }
