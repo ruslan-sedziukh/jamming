c1a3bfe5c9067fbb7324e2e5ffa089be24e18a89
Ruslan Sedziukh <ruslansedziukh@Katerynas-MacBook-Pro.local>
2022-05-13 16:26:44 +0300

    83 updated

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,31 +1,34 @@
 let accessToken; 
-const clientID;
+const clientID = '3c829511e489443e811aca63d18c2549';
 const redirectURI = "http://localhost:3000/";
 
 let Spotify = {
   getAccessToken: function () {
     if(accessToken) {
-
+      return accessToken;
     }
     else {
       let urlToken = window.location.href.match(/access_token=([^&]*)/);
-      let tokenExpiration = window.location.href.match(/expires_in=([^&]*)/)
+      let tokenExpiration = window.location.href.match(/expires_in=([^&]*)/);
 
-      if(urlToken && urlExpiration);{
+      if(urlToken && urlExpiration){
         accessToken = urlToken;
         window.setTimeout(() => {
           accessToken = '';
         }, tokenExpiration * 1000);
         window.history.pushState('Access Token', null, '/'); // what is it doing? 
-
+      }
+      else {
         // !!! there is the chance that the code below should be in another place !!!
         window.location(`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`); 
       }
     }
+  },
+
+  search: function(){
+
   }
 };
-
-
 
 
 
